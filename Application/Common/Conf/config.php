929f2db5bb1a25e14600a1d1e266a840d295a949
Pengge Zhuang <taweisuode@users.noreply.github.com>
2015-09-10 10:21:49 +0800

    Update config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -3,11 +3,11 @@ return array(
 	//'配置项'=>'配置值'
 	'SHOW_ERROR_MSG'        =>  true,    // 显示错误信息
 	'DB_TYPE'=>'mysql',
-	'DB_HOST'=>'localhost',
-	'DB_NAME'=>'pengge-root',
-	'DB_USER'=>'root',
-	'DB_PWD'=>'vertrigo',
-	'DB_PORT'=>'3306',
+	'DB_HOST'=>'******',
+	'DB_NAME'=>'******',
+	'DB_USER'=>'******',
+	'DB_PWD'=>'********',
+	'DB_PORT'=>'*****',
 	'DB_PREFIX'=>'',	// 表前缀
 	'SHOW_PAGE_TRACE'=>true,
 	//'TMPL_EXCEPTION_FILE'=>'./Application/Home/View/Public/404/index.html',// 定义公共错误模板
